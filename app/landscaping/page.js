ecd1a19a3d1785db521158af6018b489a06a2555
00gigs <1onlinegigs@gmail.com>
2024-03-16 17:20:17 -0400

    updated landscaping page

--- app/landscaping/page.js
+++ app/landscaping/page.js
@@ -6,7 +6,31 @@ const page = () => {
     <div className='min-h-screen bg-slate-100'>
         <Navbar/>
         <div className='text-black'>
-            landscape
+<div className='border-t-4 border-b-4 m-2'>
+  <div className='flex-1'>
+<img className='float-right w-50 h-60 object-cover mx-3 my-2 border-2 border-black rounded' src='https://images.unsplash.com/photo-1605117882932-f9e32b03fea9?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFuZHNjYXBpbmd8ZW58MHx8MHx8fDA%3D'/>
+  <h1 className='font-bold text-sm'>"HANZ DELIVERS UNMATCHED LANDSCAPE SERVICES WITH A PERSONAL TOUCH."</h1>
+  <p className='font-light text-[26px]'>Transform Your Outdoors into a Personal Oasis with Hanz! Ever dreamt of a beautifully landscaped garden, a lush, vibrant lawn, or enchanting outdoor spaces that beckon for leisurely afternoons? Look no further than Hanz for all your landscaping needs. Our expert contractors specialize in earth sculpting, mulching, laying the perfect sod, transporting rocks, and detailed shovel and wheelbarrow work. We're not just transforming land; we're crafting your vision into reality, from the installation of elegant landscape rocks to atmospheric lighting. Let Hanz turn your outdoor spaces into stunning extensions of your home, where every moment becomes a cherished memory. With Hanz, your dream landscape is within reach.</p>
+  </div>
+</div>   
+<div className='border-b-4 m-2'>
+<h1 className='font-bold text-sm'>"OUR SKILLED CONTRACTORS OFFER COST-EFFECTIVE, HIGH-QUALITY LANDSCAPING AND GARDEN CARE"</h1>
+  <p className='font-light text-[26px]'>At Hanz, we bridge the gap between you and top-tier residential and commercial landscape labor contractors, as well as full-service landscape professionals. Our dedicated yard work specialists are equipped to handle a wide array of tasks, ensuring your outdoor spaces are not just maintained, but truly flourish. Our services include:
+  <ul className='text-[20px] italic'>
+    <li>- Excavating trenches </li>
+    <li>- Sculpting  and maintaining hedges</li>
+    <li>- Precision lawn mowing</li>
+    <li>- Expert mulching</li>
+    <li>- Comprehensive leaf cleanup  </li>
+    <li>- Effective weed control   </li>
+    <li>- Safe stump extraction</li>
+    <li>- Strategic tree installation ...and much more!</li>
+  </ul>
+ Let Hanz connect you with the expertise you need to transform and maintain your outdoor areas, creating spaces that reflect your aesthetic and functional needs</p>
+    
+</div>
+
+<hr/>
             </div>
       
     </div>
