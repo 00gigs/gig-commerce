5e36f9039b8555696ed516d20d1a5e335983632b
00gigs <1onlinegigs@gmail.com>
2024-04-30 17:24:47 -0400

    middleware

--- middleware.js
+++ middleware.js
@@ -1,3 +1,3 @@
 export {default} from 'next-auth/middleware'
 
-export const config = {matcher: ['https://www.hanzhomesolutions.com/']};
+export const config = {matcher: ["https://www.hanzhomesolutions.com/"]};
