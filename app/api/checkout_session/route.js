b12a791c2851b928c56595d7bb39eeda3a9f09af
00gigs <1onlinegigs@gmail.com>
2024-05-08 10:17:22 -0400

    stripe updated

--- app/api/checkout_session/route.js
+++ app/api/checkout_session/route.js
@@ -23,8 +23,8 @@ const customer = await stripe.customers.create({
         ],
         mode: 'payment',
         customer_email:customer.email,
-        success_url:'http://localhost:3000/ThankYou' ,
-        cancel_url: 'http://localhost:3000/Payment'
+        success_url:'https://www.hanzhomesolutions.com/ThankYou' ,
+        cancel_url: 'https://www.hanzhomesolutions.com/Payment'
       });
       return new NextResponse(JSON.stringify({ url: session.url }), {
         status: 200, // Use 200 OK status when successfully creating a session
