49882a9f5fe9968b533ad52e251532e6ff469c65
00gigs <1onlinegigs@gmail.com>
2024-05-28 15:53:50 -0400

    changed destination in matcher

--- middleware.js
+++ middleware.js
@@ -7,7 +7,7 @@ export const config = {
         source: '/',
         has: [{ type: 'header', key: 'cookie', value: 'session' }], // Check if the request has a session cookie
         missing: [{ type: 'header', key: 'cookie', value: 'session' }],
-        destination:'/Login',
+        destination:'https://www.hanzhomesolutions.com/Login',
         permanent:false, // Redirect if the request does not have a session cookie
       },
     ],
