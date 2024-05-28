365e4ed499b51361bbee755daa27eb2f74ec3386
00gigs <1onlinegigs@gmail.com>
2024-05-28 17:01:26 -0400

    added bg logo on login

--- app/component/LoginForm.jsx
+++ app/component/LoginForm.jsx
@@ -40,7 +40,7 @@ const handleSubmit = async (e)=>{
 
   return (
     <div>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-[url('Black And White Aesthetic Minimalist Modern Simple Typography Coconut Cosmetics Logo.png')]">
+      <div className="flex flex-col items-center justify-center min-h-screen bg-[url('/Black And White Aesthetic Minimalist Modern Simple Typography Coconut Cosmetics Logo.png')] bg-cover bg-center">
       <h1 className="mb-4 text-4xl font-bold">welcome</h1>
         <div className="rounded-lg shadow-lg text-center border-t-4  border-yellow-300 flex-col flex p-3 items-center max-w-72  bg-slate-700">
           <h1>Login</h1>
