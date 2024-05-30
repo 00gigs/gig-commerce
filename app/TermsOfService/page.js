18fa2fae33630d893afedd8a01eaae4d6d76b20e
00gigs <1onlinegigs@gmail.com>
2024-05-30 13:13:07 -0400

    fixed alignment on terms page

--- app/TermsOfService/page.js
+++ app/TermsOfService/page.js
@@ -8,50 +8,54 @@ const page = () => {
             <br />
             <br />
             <h1 className='font-bold'>1. Scope of Work:</h1>
-            HanzHomeSolutions operates a platform connecting skilled freelancers in landscaping, handyman work, moving, and cleaning services with customers seeking such services. By agreeing to this Agreement, You agree to be listed on the HanzHomeSolutions platform as a Freelancer, subject to the terms and conditions herein.
+            <p className='text-center'>HanzHomeSolutions operates a platform connecting skilled freelancers in landscaping, handyman work, moving, and cleaning services with customers seeking such services. By agreeing to this Agreement, You agree to be listed on the HanzHomeSolutions platform as a Freelancer, subject to the terms and conditions herein.</p>
             <br />
             <br />
             <h1 className='font-bold'>2. Independent Contractor Status:</h1>
-            You acknowledge and agree that You are an independent contractor and not an employee, partner, or joint venture of HanzHomeSolutions. As an independent contractor, You are responsible for Your own taxes, insurance, and compliance with local laws and regulations.
+            <p className='text-center'>You acknowledge and agree that You are an independent contractor and not an employee, partner, or joint venture of HanzHomeSolutions. As an independent contractor, You are responsible for Your own taxes, insurance, and compliance with local laws and regulations.</p>
 
             <br />
             <br />
             <h1 className='font-bold'>3. Screening and Approval:</h1>
-            HanzHomeSolutions reserves the right to screen and approve Freelancers based on their qualifications, experience, and background. By accepting this Agreement, You authorize HanzHomeSolutions to conduct background checks, verify references, and perform any other necessary screening procedures.
-
+            <p className='text-center'>HanzHomeSolutions reserves the right to screen and approve Freelancers based on their qualifications, experience, and background. By accepting this Agreement, You authorize HanzHomeSolutions to conduct background checks, verify references, and perform any other necessary screening procedures.</p>
             <br />
             <br />
             <h1 className='font-bold'>4. Services and Payment:</h1>
-            a. Once approved, You will be in the HanzHomeSolutions DataBase and may be dispatched to provide services to customers who book through the platform.
-            b. You agree to provide services in a timely, professional, and satisfactory manner, consistent with industry standards.
-            c. Payment for services rendered will be made by the customer through the HanzHomeSolutions platform.
+            <ul className='text-center'>
+                <li> a. Once approved, You will be in the HanzHomeSolutions DataBase and may be dispatched to provide services to customers who book through the platform.</li>
+                <li> b. You agree to provide services in a timely, professional, and satisfactory manner, consistent with industry standards.</li>
+                <li> c. Payment for services rendered will be made by the customer through the HanzHomeSolutions platform.</li>
+            </ul>
+           
+          
+            
 
             <br />
             <br />
             <h1 className='font-bold'>5. Non-Compete and Confidentiality:</h1>
-            During the term of this Agreement and for a period of one (1) year thereafter, You agree not to directly or indirectly compete with HanzHomeSolutions or solicit its customers for competing services. You also agree to maintain the confidentiality of any proprietary information or trade secrets of HanzHomeSolutions.
+            <p className='text-center'>During the term of this Agreement and for a period of one (1) year thereafter, You agree not to directly or indirectly compete with HanzHomeSolutions or solicit its customers for competing services. You also agree to maintain the confidentiality of any proprietary information or trade secrets of HanzHomeSolutions.</p>
 
             <br />
             <br />
             <h1 className='font-bold'>6. Indemnification:</h1>
-            You agree to indemnify and hold harmless HanzHomeSolutions, its officers, directors, employees, and agents from any claims, damages, liabilities, or expenses arising out of or related to Your performance of services under this Agreement.
+            <p className='text-center'>You agree to indemnify and hold harmless HanzHomeSolutions, its officers, directors, employees, and agents from any claims, damages, liabilities, or expenses arising out of or related to Your performance of services under this Agreement.</p>
 
             <br />
             <br />
             <h1 className='font-bold'>7. Termination:</h1>
-            Either party may terminate this Agreement at any time for any reason upon written notice to the other party. Upon termination, You will cease to be listed as a Freelancer on the HanzHomeSolutions platform.
+            <p className='text-center'>Either party may terminate this Agreement at any time for any reason upon written notice to the other party. Upon termination, You will cease to be listed as a Freelancer on the HanzHomeSolutions platform.</p>
 
             <br />
             <br />
             <h1 className='font-bold'>8. Governing Law and Dispute Resolution:</h1>
-            This Agreement shall be governed by and construed in accordance with the laws of [Insert Jurisdiction]. Any dispute arising out of or related to this Agreement shall be resolved through arbitration in [Insert City], in accordance with the rules of the American Arbitration Association.
+            <p className='text-center'>This Agreement shall be governed by and construed in accordance with the laws of Maryland. Any dispute arising out of or related to this Agreement shall be resolved through arbitration in Baltimore, in accordance with the rules of the American Arbitration Association.</p>
 
             <br />
             <br />
             <h1 className='font-bold'>9. Entire Agreement:</h1>
             <br />
             <br />
-            <div className='font-bold'>This Agreement constitutes the entire understanding between the parties with respect to the subject matter hereof and supersedes all prior agreements and understandings, whether written or oral.
+            <div className='font-bold text-center'>This Agreement constitutes the entire understanding between the parties with respect to the subject matter hereof and supersedes all prior agreements and understandings, whether written or oral.
                 By accepting this Agreement, You acknowledge that You have read, understood, and agree to be bound by its terms and conditions.</div>
         </div>
     )
