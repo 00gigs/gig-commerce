00d666b2baf62df0d77f9534c4b030491bf47763
00gigs <1onlinegigs@gmail.com>
2024-07-03 15:05:21 -0400

    sms dis fixed

--- app/sms/page.js
+++ app/sms/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <p className=' font-extrabold'>
+      <p className=' font-extrabold grid justify-items-center border-2 py-5 border-black text-black min-h-screen'>
       By submitting this form you agree to receive SMS from Harrington Cyber. Carrier and Data rates may apply. Message frequency may vary Reply STOP at any time to end messaging or Reply Help for more information or you can say any information entered on this page will not be used to initiate SMS
       </p>
     </div>
