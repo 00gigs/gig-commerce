9e562294c085f4db91d881022c22dcdd42013e04
00gigs <1onlinegigs@gmail.com>
2024-07-13 00:00:01 -0400

    footer added again

--- app/component/Footer.js
+++ app/component/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="bg-amber-400 w-full h-14 col-span-3 text-center p-3">
-      <ul className='mx-3'>
+    <div className="bg-amber-400 w-full h-14 text-center p-3">
+      <ul className='mx-3 flex justify-between'>
         <li className="hover:cursor-pointer my-2 noto-sans-mono">item 1</li>
         <li className="hover:cursor-pointer my-2 noto-sans-mono">item 2</li>
         <li className="hover:cursor-pointer my-2 noto-sans-mono">item 3</li>
