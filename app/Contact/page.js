40c6786ce78ce52ae403421f49209576287bcb57
00gigs <1onlinegigs@gmail.com>
2024-09-08 15:42:17 -0400

    about text content final

--- app/Contact/page.js
+++ app/Contact/page.js
@@ -58,8 +58,8 @@ const username = user.user.email;
     <div className="bg-slate-100 w-screen min-h-screen flex flex-col">
       <Navbar/>
       <div className='flex-grow flex flex-col justify-center items-center text-black text-center'>
-       <div className='noto-znamenny text-xl mb-52 mt-9'>
        <h1 className='mt-2 opacity-40'>CONTACT PAGE</h1>
+       <div className='noto-znamenny text-xl mb-52 mt-9'>
        Need more help booking ? Speak with a representative. To get started  click " Call Now " 📞<a className=' bg-orange-300 rounded-xl p-2 hover:cursor-pointer' href='tel:+8446945262'>Call Now</a>
         </div>
 
