2772acdd22d63c972386593836e42c05dd080485
00gigs <1onlinegigs@gmail.com>
2024-09-10 22:38:54 -0400

    css

--- app/Moving/page.js
+++ app/Moving/page.js
@@ -15,7 +15,7 @@ const Page = () => {
 <div className=' border-black mx-8  p-3 text-center mt-4'>
   <div className='flex-1 mb-8'>
 <img className='float-right w-48 h-40 object-cover mx-3 my-2 border-2 border-black rounded' src='https://images.pexels.com/photos/4246266/pexels-photo-4246266.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'/>
-  <h1 className='align-middle font-bold text-sm mb-4 noto-sans-indic capitalize text-[#463fcf] p-2 border-b-4 border-gray-500'>Reliable Moving Services for Property Managers, Homeowners, and Renters in Baltimore, MD</h1>
+  <h1 className='align-middle font-bold text-sm mb-4 noto-sans-indic capitalize text-[#43d445d4] p-2 border-b-4 border-gray-500'>Reliable Moving Services for Property Managers, Homeowners, and Renters in Baltimore, MD</h1>
   <p className='font-light text-[26px] noto-znamenny'>At Hanz Home-Solutions, we provide dependable and efficient moving services tailored for property managers, homeowners, renters, and real estate investors in the Baltimore, MD area. Whether you’re moving into a new home, relocating an office, or managing tenant move-ins/outs, our experienced team offers affordable, high-quality moving services to make your transition smooth and stress-free.</p>
   </div>
 </div>   
@@ -28,7 +28,7 @@ const Page = () => {
 <h2 className='font-bold text-sm mb-4 noto-sans-indic'>Hassle-Free Moving Services for Homes and Businesses</h2>
 <p className='font-light text-[26px] noto-znamenny'>Our skilled team at Hanz is here to make your next move as seamless as possible. From loading and unloading to packing and unpacking, we handle every detail of your move with efficiency and care. Whether you’re moving locally or relocating your business, trust Hanz to get your belongings safely from one location to another. We specialize in providing cost-effective, reliable solutions for residential and commercial moving.</p>
   <img className='float-left w-48 h-52 object-cover mx-3 my-2 border-4 border-black rounded' src='https://images.pexels.com/photos/5025512/pexels-photo-5025512.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'/>
- <h3 className='font-light text-[16px] italic  rounded-xl noto-znamenny bg-[#4457d2] p-2 border-b-4 border-gray-500'>Moving Services We Provide</h3> 
+ <h3 className='font-light text-[16px] italic  rounded-xl noto-znamenny bg-[#31ad33d4] p-2 border-b-4 border-gray-500'>Moving Services We Provide</h3> 
   <ul className='text-[20px] italic noto-sans-indic'>
     <li className="list-disc my-3 font-light text-[20px] leading-10   tracking-tight">Loading and Unloading: Expertly moving your items onto and off of the truck with precision.</li>
     <li className="list-disc my-3 font-light text-[20px] leading-10   tracking-tight">Packing and Unpacking: Careful packing of your items to prevent damage and unpacking them upon arrival.</li>
@@ -36,7 +36,7 @@ const Page = () => {
     <li className="list-disc my-3 font-light text-[20px] leading-10   tracking-tight">Furniture Assembly and Disassembly: We handle the assembly and disassembly of your furniture for easy transport.</li>
     <li className="list-disc my-3 font-light text-[20px] leading-10   tracking-tight">Furniture Placement: Our team ensures your furniture is arranged exactly to your specifications in your new location.</li>
   </ul>
-<h4 className='font-light text-[16px] italic  rounded-xl noto-znamenny bg-[#4457d2] p-2 border-b-4 border-gray-500'>Why Property Managers and Homeowners Choose Hanz Home-Solutions</h4>
+<h4 className='font-light text-[16px] italic  rounded-xl noto-znamenny bg-[#31ad33d4] p-2 border-b-4 border-gray-500'>Why Property Managers and Homeowners Choose Hanz Home-Solutions</h4>
 <ul>
   <li className="list-disc my-3 font-light text-[20px] leading-10   tracking-tight">Efficient Moving Services: We offer a full range of moving services, perfect for property managers needing fast turnover or homeowners moving into new spaces.</li>
   <li className="list-disc my-3 font-light text-[20px] leading-10   tracking-tight">Affordable Rates: We provide competitive pricing, ensuring that your move is not only smooth but also cost-effective.</li>
