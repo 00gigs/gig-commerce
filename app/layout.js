378a9e4e8b847a01068f3b627a6dc7db54579736
00gigs <1onlinegigs@gmail.com>
2024-09-12 14:38:06 -0400

    fb pixel script

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./Providers";
 import Head from "next/head";
+import Script from "next/script";
 import { GoogleAnalytics } from "@next/third-parties/google";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -84,6 +85,31 @@ export default function RootLayout({ children, session }) {
 
       <body className={inter.className}>
         <AuthProvider>{children}</AuthProvider>
+        <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+      >
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', 'YOUR_PIXEL_ID');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=829980235980500&ev=PageView&noscript=1`}
+        />
+      </noscript>
       </body>
       <GoogleAnalytics gaId="G-G273KV5KZ1" />
     </html>
