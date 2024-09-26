57aafce09f317ca0da4e4e567f5a153908efb278
00gigs <1onlinegigs@gmail.com>
2024-09-26 15:33:20 -0400

    fix last push

--- middleware.js
+++ middleware.js
@@ -1,28 +1,28 @@
-// import { headers } from "next/headers";
-// import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
 
-// export const config = {
-//     matcher: [
-//       '/Login',
-//       '/'
-//     ],
-//   };
+export const config = {
+    matcher: [
+      '/Login',
+      '/'
+    ],
+  };
 
-//   export function middleware(request){
-//     const {pathname} = request.nextUrl;
-//     const cookie = request.headers.get('cookie');
-//     const hasSession = cookie && cookie.includes('session');
+  export function middleware(request){
+    const {pathname} = request.nextUrl;
+    const cookie = request.headers.get('cookie');
+    const hasSession = cookie && cookie.includes('session');
 
 
-//     if(!hasSession && pathname !== '/Login'){
-// return NextResponse.redirect(new URL('/Login', request.url))
-//     };
+    if(!hasSession && pathname !== '/Login'){
+return NextResponse.redirect(new URL('/Login', request.url))
+    };
 
-//     if(hasSession && pathname === '/Login'){
-// return NextResponse.next()
-//     };
+    if(hasSession && pathname === '/Login'){
+return NextResponse.next()
+    };
 
-// return NextResponse.next()
-//   }
+return NextResponse.next()
+  }
 
   
